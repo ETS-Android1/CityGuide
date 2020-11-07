70a1d7331ff9c41bcb6bab62a40178bf1d066998
P Sai Deekshith <56923148+deekshith39@users.noreply.github.com>
2020-11-07 20:22:31 +0530

    --

--- app/src/main/java/com/example/cityguide/User/UserDashboard.java
+++ app/src/main/java/com/example/cityguide/User/UserDashboard.java
@@ -1,19 +1,18 @@
 package com.example.cityguide.User;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.Gravity;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.WindowManager;
-import android.widget.ImageView;
 
 import com.example.cityguide.Common.LoginSinup.OwnerStartupPage;
 import com.example.cityguide.HelperClasses.Home.CategoriesAdapter;
